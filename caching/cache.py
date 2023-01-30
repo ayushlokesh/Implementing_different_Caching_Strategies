d71ee7dc6d1e3bc2a6a171eeefd1dc6143cb7cce
sean.bechhofer <sean.bechhofer@manchester.ac.uk>
2023-01-30 16:41:50 +0000

    Updating cache boiler plate

--- caching/cache.py
+++ caching/cache.py
@@ -8,20 +8,23 @@ class AbstractCache(Memory):
     def name(self):
         return "Cache"
 
+    # Takes two parameters. Data is the data that forms the
+    # "memory". Size is a non-negative integer that indicates the size
+    # of the cache.
     def __init__(self, data, size=5):
         super().__init__(data)
         self.cache_hit_count = 0
 
     # Returns information on the number of cache hit counts
     def get_cache_hit_count(self):
-        # In this class, always returns 0 as there is no cache.
-        return self.cache_hit_count
+        # TODO: Edit this to correctly return the count of cache hits.
+        return None
     
 class CyclicCache(AbstractCache):
     def name(self):
         return "Cyclic"
 
-    # Edit the code below to provide an implementation of a cache that
+    # TODO: Edit the code below to provide an implementation of a cache that
     # uses a cyclic caching strategy with the given cache size. You
     # can use additional methods and variables as you see fit as long
     # as you provide a suitable overridding of the lookup method.
@@ -34,7 +37,7 @@ class LRUCache(AbstractCache):
     def name(self):
         return "LRU"
 
-    # Edit the code below to provide an implementation of a cache that
+    # TODO: Edit the code below to provide an implementation of a cache that
     # uses a least recently used caching strategy with the given cache size.
     # You can use additional methods and variables as you see fit as
     # long as you provide a suitable overridding of the lookup method.
@@ -47,7 +50,7 @@ class RandomCache(AbstractCache):
     def name(self):
         return "Random"
 
-    # Edit the code below to provide an implementation of a cache that
+    # TODO: Edit the code below to provide an implementation of a cache that
     # uses a random eviction strategy with the given cache size.
     # You can use additional methods and variables as you see fit as
     # long as you provide a suitable overridding of the lookup method.
